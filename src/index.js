409666fec02014e5d5d6e517f4dbd81761698268
ADyBaH <Brandon4@mail.ru>
2023-01-16 13:13:15 +0300

    feat: add solution for brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) return false;
+  let flag = true;
+  bracketsConfig = bracketsConfig.map(e => e.join(''));
+  while(flag) {
+    for(const bracket of bracketsConfig) {
+        if(str.includes(bracket)) {
+          str = str.replace(bracket, '')
+        } else {
+          if(!bracketsConfig.some(e => str.includes(e))) flag = false;
+        }
+    }
+  }
+  if(str.length !== 0) return false;
+  return true;
 }
